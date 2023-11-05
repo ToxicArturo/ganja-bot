5bb1446b944cdff36dbb9d6ae707c64d3fb916bc
ㄒㄖ乂丨匚 卂尺ㄒㄩ尺ㄖ <140161291+ToxicArturo@users.noreply.github.com>
2023-11-06 01:08:44 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "9636ba88d645d36a3a89cd9365af0caa")
     BOT_TOKEN = getenv("BOT_TOKEN", "6646925367:AAEC3OzkoVppNjQj1kT0Goxx0bAz-eJmK2Q")
     FSUB = getenv("FSUB", "Toxic_TV_24")
-    CHID = int(getenv("CHID", "1831951100"))
+    CHID = int(getenv("CHID", "-1001831951100"))
     SUDO = list(map(int, getenv("SUDO", "1358626459").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Arturo:Arturo_pro@cluster0.zvn1iwr.mongodb.net/?retryWrites=true&w=majority")
     
